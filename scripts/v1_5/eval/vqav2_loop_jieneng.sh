c6b68621be0fa1f04354050a45809add6a3daf3a
Beckschen <jienengchen01@gmail.com>
2024-03-21 01:50:28 -0400

    vqav2_loop_jieneng.sh runnable

--- scripts/v1_5/eval/vqav2_loop_jieneng.sh
+++ scripts/v1_5/eval/vqav2_loop_jieneng.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
-
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 gpu_list="${CUDA_VISIBLE_DEVICES:-0}"
 IFS=',' read -ra GPULIST <<< "$gpu_list"
 CHUNKS=${#GPULIST[@]}
 
 CKPT="my-llava-1.5-7b"
 SPLIT="llava_vqav2_mscoco_test-dev2015"
-stride=2
-layer=16
-for IDX in $(seq 0 $((CHUNKS-1))); do
-    CUDA_VISIBLE_DEVICES=${GPULIST[$IDX]} python -m llava.eval.model_vqa_loader \
-        --model-path liuhaotian/llava-v1.5-7b \
-        --question-file ./playground/data/eval/vqav2/$SPLIT.jsonl \
-        --image-folder /data/jieneng/data/llava_datasets/eval/vqav2/test2015 \
-        --answers-file ./playground/data/eval/vqav2/answers/$SPLIT/$CKPT/stride-$stride-layer-$layer/${CHUNKS}_${IDX}.jsonl \
-        --num-chunks $CHUNKS \
-        --chunk-idx $IDX \
-        --temperature 0 \
-        --conv-mode vicuna_v1 \
-        --stride $stride \
-        --layer $layer &
+
+for stride in 2 4 8 16 32 64; do
+    for layer in 8 16 31; do
+        for IDX in $(seq 0 $((CHUNKS-1))); do
+            CUDA_VISIBLE_DEVICES=${GPULIST[$IDX]} python -m llava.eval.model_vqa_loader \
+                --model-path liuhaotian/llava-v1.5-7b \
+                --question-file ./playground/data/eval/vqav2/$SPLIT.jsonl \
+                --image-folder /data/jieneng/data/llava_datasets/eval/vqav2/test2015 \
+                --answers-file ./playground/data/eval/vqav2/answers/$SPLIT/$CKPT/stride-$stride-layer-$layer/${CHUNKS}_${IDX}.jsonl \
+                --num-chunks $CHUNKS \
+                --chunk-idx $IDX \
+                --temperature 0 \
+                --conv-mode vicuna_v1 \
+                --stride $stride \
+                --layer $layer &
+        done
+        wait
+    done
 done
 
 
