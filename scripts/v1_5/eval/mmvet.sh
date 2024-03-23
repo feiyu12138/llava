16230e0f1aedc8f6299abd35473e68cb0f30e835
feiyu12138 <lye21@ccvl27.ccvl.jhu.edu>
2024-03-23 17:57:59 -0400

    2d runable

--- scripts/v1_5/eval/mmvet.sh
+++ scripts/v1_5/eval/mmvet.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-
+layer=16
+stride=2
+grouping=none
 python -m llava.eval.model_vqa \
     --model-path my-llava-1.5-7b \
     --question-file /home/lye21/LLaVA/playground/data/eval/mm-vet/llava-mm-vet.jsonl \
     --image-folder /data/jieneng/data/llava_datasets/eval/mmvet/mm-vet/images \
-    --answers-file ./playground/data/eval/mm-vet/answers/llava-v1.5-7b.jsonl \
+    --answers-file ./playground/data/eval/mm-vet/answers/llava-v1.5-7b-stride-$stride-layer-$layer-grouping-$grouping.jsonl \
     --temperature 0 \
     --conv-mode vicuna_v1 \
-    --layer 16 \
-    --stride 2 \
-    --grouping avgpool1d
+    --layer $layer \
+    --stride $stride \
+    --grouping $grouping 
 
 mkdir -p ./playground/data/eval/mm-vet/results
 
