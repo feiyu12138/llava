36fbb5213a46622e6c80d688a389a7b6b0aa46f4
Beckschen <jienengchen01@gmail.com>
2024-03-28 15:41:26 -0400

    a

--- scripts/v1_5/finetune_ccvl29_pool4layer8half.sh
+++ scripts/v1_5/finetune_ccvl29_pool4layer8half.sh
@@ -66,7 +66,7 @@ deepspeed llava/train/train_mem.py \
     --data_path /data/datasets/jchen293/data/llava_datasets/LLaVA-Tuning/llava_v1_5_mix665k.json \
     --image_folder /data/datasets/jchen293/data/llava_datasets/LLaVA-Tuning \
     --vision_tower openai/clip-vit-large-patch14-336 \
-    --pretrain_mm_mlp_adapter /datasets/jchen293/weights/llava/checkpoint/llava-v1.5-7b-pretrain-stride-$stride-layer-$layer-grouping-$grouping-half/mm_projector.bin \
+    --pretrain_mm_mlp_adapter /data/datasets/jchen293/weights/llava/checkpoint/llava-v1.5-7b-pretrain-stride-$stride-layer-$layer-grouping-$grouping-half/mm_projector.bin \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
