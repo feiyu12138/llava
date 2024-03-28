4cd5f598ffdaeb9bf459f77948d92a453844719a
jchen293 <jchen293@ccvl28.ccvl.jhu.edu>
2024-03-28 01:06:14 -0400

    Feat: add half pool

--- scripts/v1_5/eval/sqa.sh
+++ scripts/v1_5/eval/sqa.sh
@@ -4,7 +4,7 @@ python -m llava.eval.model_vqa_science \
     --model-path liuhaotian/llava-v1.5-7b \
     --question-file ./playground/data/eval/scienceqa/llava_test_CQM-A.json \
     --image-folder /data/jieneng/data/llava_datasets/eval/scienceqa/ScienceQA/test \
-    --answers-file ./playground/data/eval/scienceqa/answers/llava-v1.5-7b.jsonl \
+    --answers-file ./playground/data/eval/scienceqa/answers/llava-v1.5-7b-.jsonl \
     --single-pred-prompt \
     --temperature 0 \
     --conv-mode vicuna_v1
