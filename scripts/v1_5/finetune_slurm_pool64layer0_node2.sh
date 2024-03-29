5226d81959fc19257cb5edf5a2db9962a0b288b4
jchen293 <jchen293@login.ccvl.jhu.edu>
2024-03-29 12:26:08 -0400

    update bash

---
+++ scripts/v1_5/finetune_slurm_pool64layer0_node2.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+#
+#SBATCH --job-name=pool64layer0_node2
+#SBATCH --error=/datasets/jchen293/logs/exp/llava/pool64layer0_node2.err
+#SBATCH --output=/datasets/jchen293/logs/exp/llava/pool64layer0_node2.out
+#SBATCH --gpus-per-node=8
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=8
+#SBATCH --open-mode=append
+#SBATCH --partition=main
+#SBATCH --exclude=ccvl14
+
+export WANDB_API_KEY='70c34ec6ff006f3a8b19234dd103f67feed8083b'
+export WANDB_PROJECT='llava'
+
+export MASTER_PORT=12802
+master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+export MASTER_ADDR=$master_addr
+
+module purge
+module load conda
+conda activate llava_git
+
+layer=0
+stride=64
+grouping=avgpool1d
+ROOT_DATA=/datasets/jchen293/data/llava_datasets
+ROOT_WEIGHT=/datasets/jchen293/weights/llava/checkpoint
+
+deepspeed llava/train/train_mem.py \
+    --deepspeed ./scripts/zero3.json \
+    --model_name_or_path lmsys/vicuna-7b-v1.5 \
+    --version v1 \
+    --data_path $ROOT_DATA/LLaVA-Tuning/llava_v1_5_mix665k.json \
+    --image_folder $ROOT_DATA/LLaVA-Tuning \
+    --vision_tower openai/clip-vit-large-patch14-336 \
+    --pretrain_mm_mlp_adapter $ROOT_WEIGHT/llava-v1.5-7b-pretrain-stride-$stride-layer-$layer-grouping-$grouping/mm_projector.bin \
+    --mm_projector_type mlp2x_gelu \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --image_aspect_ratio pad \
+    --group_by_modality_length True \
+    --bf16 True \
+    --output_dir $ROOT_WEIGHT/llava-v1.5-7b-stride-$stride-layer-$layer-grouping-$grouping \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 2 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 50000 \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 4 \
+    --lazy_preprocess True \
+    --report_to wandb \
+    --run_name pool64layer0_node2 \
+    --stride $stride \
+    --layer $layer \
+    --grouping $grouping
