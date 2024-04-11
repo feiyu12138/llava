3d90e0605ffd683ecccf917b36a721e578a5a656
jchen293 <jchen293@ccvl28.ccvl.jhu.edu>
2024-04-11 17:52:17 -0400

    eval bash

--- llava/eval/model_vqa_loader.py
+++ llava/eval/model_vqa_loader.py
@@ -82,6 +82,9 @@ def eval_model(args):
     model_path = os.path.expanduser(args.model_path)
     model_name = get_model_name_from_path(model_path)
     tokenizer, model, image_processor, context_len = load_pretrained_model(model_path, args.model_base, model_name)
+    
+    model.cot_decoding = args.cot_decoding
+    model.num_branch = args.num_branch
 
     questions = [json.loads(q) for q in open(os.path.expanduser(args.question_file), "r")]
     questions = get_chunk(questions, args.num_chunks, args.chunk_idx)
@@ -139,6 +142,10 @@ if __name__ == "__main__":
     parser.add_argument("--top_p", type=float, default=None)
     parser.add_argument("--num_beams", type=int, default=1)
     parser.add_argument("--max_new_tokens", type=int, default=128)
+    parser.add_argument("--icl", action="store_true")
+    parser.add_argument("--icl-file", type=str, default="none")
+    parser.add_argument("--cot-decoding", action="store_true")
+    parser.add_argument("--num-branch", type=int, default=4)
     args = parser.parse_args()
 
     eval_model(args)
