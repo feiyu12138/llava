6c87638c20b18ccae485c4636ee732ac1831a9d1
feiyu12138 <204688451@qq.com>
2024-04-28 23:09:12 -0400

    fastv

--- scripts/v1_5/eval/mme_stride_8_layer_2_grouping_1d.sh
+++ scripts/v1_5/eval/mme_stride_8_layer_2_grouping_1d.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=5
-name=llava-v1.5-7b-fastv-layer2-rank72-inplace
-rank=72
-k=2
+export CUDA_VISIBLE_DEVICES=6
+layer=2
+stride=8
+grouping=avgpool1d
+name=llava-v1.5-7b-stride-$stride-layer-$layer-grouping-$grouping
+CKPT="/home/lye21/LLaVA/checkpoints/llava-v1.5-7b-$name"
 python -m llava.eval.model_vqa_loader \
     --model-path liuhaotian/llava-v1.5-7b \
     --question-file ./playground/data/eval/MME/llava_mme.jsonl \
@@ -10,12 +12,9 @@ python -m llava.eval.model_vqa_loader \
     --answers-file ./playground/data/eval/MME/answers/$name.jsonl \
     --temperature 0 \
     --conv-mode vicuna_v1 \
-    --use-fast-v True \
-    --fast-v-sys-length 36 \
-    --fast-v-image-token-length 576 \
-    --fast-v-attention-rank $rank \
-    --fast-v-agg-layer $k \
-
+    --stride $stride \
+    --layer $layer \
+    --grouping $grouping
 
 cd ./playground/data/eval/MME
 
@@ -23,4 +22,4 @@ python convert_answer_to_mme.py --experiment $name
 
 cd eval_tool
 
-python calculation.py --results_dir answers/$name
+python calculation.py --results_dir answers/$name > ./eval_result/$name.txt
--- scripts/v1_5/eval/mmvet_pool1d_layer2_stride_8.sh
+++ scripts/v1_5/eval/mmvet_pool1d_layer2_stride_8.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=1
-name=llava-v1.5-7b-fastv-layer2-rank72
-rank=72
-k=2
+layer=2
+stride=8
+grouping=avgpool1d
+name=llava-v1.5-7b-stride-8-layer-2-grouping-avgpool1d
 python -m llava.eval.model_vqa \
     --model-path liuhaotian/llava-v1.5-7b \
     --question-file ./playground/data/eval/mm-vet/llava-mm-vet.jsonl \
@@ -10,11 +11,9 @@ python -m llava.eval.model_vqa \
     --answers-file ./playground/data/eval/mm-vet/answers/$name.jsonl \
     --temperature 0 \
     --conv-mode vicuna_v1 \
-    --use-fast-v True \
-    --fast-v-sys-length 36 \
-    --fast-v-image-token-length 576 \
-    --fast-v-attention-rank $rank \
-    --fast-v-agg-layer $k 
+    --layer $layer \
+    --stride $stride \
+    --grouping $grouping 
 
 mkdir -p ./playground/data/eval/mm-vet/results
 
