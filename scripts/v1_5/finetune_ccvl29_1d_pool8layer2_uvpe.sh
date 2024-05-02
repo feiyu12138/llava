e336952ae8c7971a105dda0261a0eb016a618611
Beckschen <jienengchen01@gmail.com>
2024-05-02 05:28:16 -0400

    update script

--- scripts/v1_5/finetune_ccvl29_1d_pool8layer2_uvpe.sh
+++ scripts/v1_5/finetune_ccvl29_1d_pool8layer2_uvpe.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-export NCCL_P2P_DISABLE=1
+# export NCCL_P2P_DISABLE=1
 export WANDB_API_KEY='70c34ec6ff006f3a8b19234dd103f67feed8083b'
 export WANDB_PROJECT='llava_team'
 
@@ -9,6 +9,53 @@ layer=2
 stride=8
 grouping=avgpool1d
 unified_vpe=True
+
+
+layer=2
+stride=8
+grouping=avgpool1d
+unified_vpe=True
+
+deepspeed llava/train/train_mem.py \
+    --deepspeed ./scripts/zero2.json \
+    --model_name_or_path lmsys/vicuna-7b-v1.5 \
+    --version plain \
+    --data_path /data/datasets/jchen293/data/llava_datasets/LLaVA-Pretrain/blip_laion_cc_sbu_558k.json \
+    --image_folder /data/datasets/jchen293/data/llava_datasets/LLaVA-Pretrain/images \
+    --vision_tower openai/clip-vit-large-patch14-336 \
+    --mm_projector_type mlp2x_gelu \
+    --tune_mm_mlp_adapter True \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --bf16 True \
+    --output_dir  /data/datasets/jchen293/weights/llava/checkpoint/llava-v1.5-7b-pretrain-stride-$stride-layer-$layer-grouping-$grouping-unified_vpe-$unified_vpe \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 32 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 24000 \
+    --save_total_limit 1 \
+    --learning_rate 1e-3 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 4 \
+    --lazy_preprocess True \
+    --report_to wandb \
+    --stride $stride \
+    --layer $layer \
+    --grouping $grouping \
+    --unified_vpe $unified_vpe \
+    > /data/datasets/jchen293/logs/exp/llava/pt_$grouping-stride-$stride-layer-$layer-uvpe-$unified_vpe.log
+
+
 deepspeed llava/train/train_mem.py \
     --deepspeed ./scripts/zero3.json \
     --model_name_or_path lmsys/vicuna-7b-v1.5 \
@@ -49,6 +96,6 @@ deepspeed llava/train/train_mem.py \
     --layer $layer \
     --grouping $grouping \
     --unified_vpe $unified_vpe \
-    > /data/luoxin/data/llava/log/$grouping-stride-$stride-layer-$layer-uvpe-$unified_vpe.log
+    > /data/datasets/jchen293/logs/exp/llava/$grouping-stride-$stride-layer-$layer-uvpe-$unified_vpe.log
 
 sleep 2d
