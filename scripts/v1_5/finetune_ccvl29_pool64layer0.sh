7fc9b9c4b54935fa6b417b29c4ffcba1eb637ff9
Beckschen <jienengchen01@gmail.com>
2024-05-03 01:16:56 -0400

    update script

--- scripts/v1_5/finetune_ccvl29_pool64layer0.sh
+++ scripts/v1_5/finetune_ccvl29_pool64layer0.sh
@@ -22,7 +22,7 @@ deepspeed llava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir /data/datasets/jchen293/weights/llava/checkpoint/llava-v1.5-7b-stride-$stride-layer-$layer-grouping-$grouping \
+    --output_dir /data/datasets/jchen293/weights/llava/checkpoint/llava-v1.5-7b-stride-$stride-layer-$layer-grouping-$grouping-v2 \
     --num_train_epochs 1 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
