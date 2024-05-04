9c2dfde3fb1d3377c81e1de8148b85616cb5781e
jchen293 <jchen293@login.ccvl.jhu.edu>
2024-05-04 13:51:56 -0400

    a

--- scripts/v1_5/finetune_slurm_detach_hard_kmeans_pool8layer2_uvpe.sh
+++ scripts/v1_5/finetune_slurm_detach_hard_kmeans_pool8layer2_uvpe.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 #
-export NCCL_P2P_DISABLE=1
+#SBATCH --job-name=pool8layer2detach_hardkmeans_uvpe
+#SBATCH --error=/datasets/jchen293/logs/exp/llava/pool8layer2detach_hardkmeans_uvpe.err
+#SBATCH --output=/datasets/jchen293/logs/exp/llava/pool8layer2detach_hardkmeans_uvpe.out
+#SBATCH --gpus=8
+#SBATCH --nodes=1
+#SBATCH --mail-type=END
+#SBATCH --mail-user=jchen293@jh.edu
+#SBATCH --partition=main
+#SBATCH --exclude=ccvl[14,33-38]
+
 export WANDB_API_KEY='70c34ec6ff006f3a8b19234dd103f67feed8083b'
-export WANDB_PROJECT='llava_team'
+export WANDB_PROJECT='llava'
+
+module purge
+module load conda
+conda activate llava_git
+
+
+ROOT_DATA=/datasets/jchen293/data/llava_datasets
+ROOT_WEIGHT=/datasets/jchen293/weights/llava/checkpoint
 
 
 layer=2
@@ -13,10 +30,10 @@ deepspeed llava/train/train_mem.py \
     --deepspeed ./scripts/zero3.json \
     --model_name_or_path lmsys/vicuna-7b-v1.5 \
     --version v1 \
-    --data_path /data/datasets/jchen293/data/llava_datasets/LLaVA-Tuning/llava_v1_5_mix665k.json \
-    --image_folder /data/datasets/jchen293/data/llava_datasets/LLaVA-Tuning \
+    --data_path /datasets/jchen293/data/llava_datasets/LLaVA-Tuning/llava_v1_5_mix665k.json \
+    --image_folder /datasets/jchen293/data/llava_datasets/LLaVA-Tuning \
     --vision_tower openai/clip-vit-large-patch14-336 \
-    --pretrain_mm_mlp_adapter /data/datasets/jchen293/weights/llava/checkpoint/llava-v1.5-7b-pretrain-stride-$stride-layer-$layer-grouping-$grouping-unified_vpe-$unified_vpe/mm_projector.bin \
+    --pretrain_mm_mlp_adapter /datasets/jchen293/weights/llava/checkpoint/llava-v1.5-7b-pretrain-stride-$stride-layer-$layer-grouping-$grouping-unified_vpe-$unified_vpe/mm_projector.bin \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
@@ -24,7 +41,7 @@ deepspeed llava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir  /data/datasets/jchen293/weights/llava/checkpoint/llava-v1.5-7b-finetune-stride-$stride-layer-$layer-grouping-$grouping-unified_vpe-$unified_vpe \
+    --output_dir  /datasets/jchen293/weights/llava/checkpoint/llava-v1.5-7b-finetune-stride-$stride-layer-$layer-grouping-$grouping-unified_vpe-$unified_vpe \
     --num_train_epochs 1 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
@@ -44,11 +61,10 @@ deepspeed llava/train/train_mem.py \
     --dataloader_num_workers 4 \
     --lazy_preprocess True \
     --report_to wandb \
-    --run_name pool8layer2detachhardkmeansuvpe \
+    --run_name pool8layer2detach_hardkmeans_uvpe \
     --stride $stride \
     --layer $layer \
     --grouping $grouping \
-    --unified_vpe $unified_vpe \
-    > /data/luoxin/data/llava/log/$grouping-stride-$stride-layer-$layer.log
+    --unified_vpe $unified_vpe
 
 sleep 2d
