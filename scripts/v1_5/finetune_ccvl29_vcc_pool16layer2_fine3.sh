89262c10703a68c8fc057f09d3679b16a87592ad
Beckschen <jienengchen01@gmail.com>
2024-05-04 19:08:12 -0400

    vcc train

--- scripts/v1_5/finetune_ccvl29_vcc_pool16layer2_fine3.sh
+++ scripts/v1_5/finetune_ccvl29_vcc_pool16layer2_fine3.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 #
-export NCCL_P2P_DISABLE=1
 export WANDB_API_KEY='70c34ec6ff006f3a8b19234dd103f67feed8083b'
-export WANDB_PROJECT='llava'
+export WANDB_PROJECT='llava_team'
+
 layer=2
 stride=16
 grouping=attn
 num_fine_blocks=3
+
+ROOT_DATA=/data/datasets/jchen293/data/llava_datasets
+ROOT_WEIGHT=/data/datasets/jchen293/weights/llava/checkpoint
+LOG_PATH=/data/datasets/jchen293/logs/exp/llava
+
 deepspeed llava/train/train_mem.py \
     --deepspeed ./scripts/zero3.json \
     --model_name_or_path lmsys/vicuna-7b-v1.5 \
     --version v1 \
-    --data_path /data/datasets/jchen293/data/llava_datasets/LLaVA-Tuning/llava_v1_5_mix665k.json \
-    --image_folder /data/datasets/jchen293/data/llava_datasets/LLaVA-Tuning \
+    --data_path $ROOT_DATA/LLaVA-Tuning/llava_v1_5_mix665k.json \
+    --image_folder $ROOT_DATA/LLaVA-Tuning \
     --vision_tower openai/clip-vit-large-patch14-336 \
-    --pretrain_mm_mlp_adapter /data/datasets/jchen293/weights/llava/checkpoint/llava-v1.5-7b-pretrain-stride-$stride-layer-$layer-grouping-$grouping/mm_projector.bin \
+    --pretrain_mm_mlp_adapter $ROOT_WEIGHT/llava-v1.5-7b-pretrain-stride-$stride-layer-$layer-grouping-$grouping/mm_projector.bin \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
@@ -22,7 +27,7 @@ deepspeed llava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir /data/datasets/jchen293/weights/llava/checkpoint/llava-v1.5-7b-stride-$stride-layer-$layer-grouping-$grouping \
+    --output_dir $ROOT_WEIGHT/llava-v1.5-7b-stride-$stride-layer-$layer-grouping-$grouping \
     --num_train_epochs 1 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
@@ -46,6 +51,6 @@ deepspeed llava/train/train_mem.py \
     --stride $stride \
     --layer $layer \
     --grouping $grouping \
-    > error.log
+    > $LOG_PATH/llava-v1.5-7b-stride-$stride-layer-$layer-grouping-$grouping.log
 
 sleep 2d
