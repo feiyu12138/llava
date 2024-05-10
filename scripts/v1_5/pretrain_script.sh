2893c417d11fc812854d1d8a89be377974466c98
jchen293 <jchen293@login.ccvl.jhu.edu>
2024-05-09 20:46:44 -0400

    add prog

---
+++ scripts/v1_5/pretrain_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#SBATCH --job-name=ptn
+#SBATCH --error=/datasets/jchen293/logs/exp/llava/pt_none1.err
+#SBATCH --output=/datasets/jchen293/logs/exp/llava/pt_none1.out
+#SBATCH --gpus=8
+#SBATCH --nodes=1
+#SBATCH --partition=main
+#SBATCH --exclude=ccvl[14,33-38]
+cd /home/jchen293/code/llava_git/llava
+for i in {1..50}
+do
+   bash scripts/v1_5/script_fresh.sh
+   sleep 1h
+done
---
+++ scripts/v1_5/script_fresh.sh
@@ -0,0 +1,3 @@
+nvidia-smi
+
+htop
