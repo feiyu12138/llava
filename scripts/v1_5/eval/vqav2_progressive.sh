3e183321f7e6cd11954e834d1301b926a20b3874
lye21 <lye21@login.ccvl.jhu.edu>
2024-05-09 20:39:06 -0400

    sync

---
+++ scripts/v1_5/eval/vqav2_progressive.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+ROOT_DATA=/data/datasets/jchen293/data/llava_datasets
+ROOT_WEIGHT=/data/datasets/jchen293/weights/llava/checkpoint
+
+name=pool1d-progressive
+SPLIT="llava_gqa_testdev_balanced"
+GQADIR="$ROOT_DATA/eval_luoxin/eval/gqa/data"
+grouping=none
+stride=8
+layer=2
+unified_vpe=False
+ckpt=$ROOT_WEIGHT/llava-v1.5-7b-finetune-stride-$stride-layer-$layer-grouping-avgpool1d-unified_vpe-$unified_vpe-progressive
+SPLIT="llava_vqav2_mscoco_test-dev2015"
+
+gpu_list="${CUDA_VISIBLE_DEVICES:-0}"
+IFS=',' read -ra GPULIST <<< "$gpu_list"
+
+CHUNKS=${#GPULIST[@]}
+
+for IDX in $(seq 0 $((CHUNKS-1))); do
+    CUDA_VISIBLE_DEVICES=${GPULIST[$IDX]} python -m llava.eval.model_vqa_loader \
+        --model-path $ckpt \
+        --question-file $ROOT_DATA/eval_luoxin/eval/vqav2/$SPLIT.jsonl \
+        --image-folder $ROOT_DATA/eval_luoxin/eval/vqav2/test2015 \
+        --answers-file $ROOT_DATA/eval_luoxin/eval/vqav2/answers/$SPLIT/$name/${CHUNKS}_${IDX}.jsonl \
+        --num-chunks $CHUNKS \
+        --chunk-idx $IDX \
+        --temperature 0 \
+        --conv-mode vicuna_v1 &
+done
+
+wait
+
+output_file=$ROOT_DATA/eval_luoxin/eval/vqav2/answers/$SPLIT/$name/merge.jsonl
+
+# Clear out the output file if it exists.
+> "$output_file"
+
+# Loop through the indices and concatenate each file.
+for IDX in $(seq 0 $((CHUNKS-1))); do
+    cat $ROOT_DATA/eval_luoxin/eval/vqav2/answers/$SPLIT/$name/${CHUNKS}_${IDX}.jsonl >> "$output_file"
+done
+
+python scripts/convert_vqav2_for_submission.py --split $SPLIT --ckpt $name
+
