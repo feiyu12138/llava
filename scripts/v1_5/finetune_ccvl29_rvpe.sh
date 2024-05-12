225b08243ed5ea70bbd225206e87fc826565d0d8
Beckschen <jienengchen01@gmail.com>
2024-05-12 15:55:21 -0400

    script

--- scripts/v1_5/finetune_ccvl29_rvpe.sh
+++ scripts/v1_5/finetune_ccvl29_rvpe.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
-NNODES=1
-GPUS=1
-PORT=29600
+#
+export WANDB_API_KEY='46e587ae4112a04da96b68ba807395204be787c9'
+export WANDB_PROJECT='llava_team'
+export WANDB_ENTITY='jchen293'
+
 
 DATA_PATH=/data/datasets/jchen293/data/llava_datasets
 WEIGHT_PATH=/data/datasets/jchen293/weights/llava/checkpoint
 
 layer=0
-stride=1
+stride=4
 grouping=pos_avg
 rpe=True
-
-torchrun --nnodes=${NNODES} --nproc_per_node=${GPUS} --master_port=${PORT} \
-     llava/train/train_mem.py \
+deepspeed llava/train/train_mem.py \
     --deepspeed ./scripts/zero3.json \
     --model_name_or_path lmsys/vicuna-7b-v1.5 \
     --version v1 \
-    --data_path ./playground/data/LLaVA-Tuning/llava_v1_5_mix665k.json \
-    --image_folder ./playground/data/LLaVA-Tuning \
+    --data_path $DATA_PATH/LLaVA-Tuning/llava_v1_5_mix665k.json \
+    --image_folder $DATA_PATH/LLaVA-Tuning \
     --vision_tower openai/clip-vit-large-patch14-336 \
-    --pretrain_mm_mlp_adapter $WEIGHT_PATH/llava-v1.5-7b-pretrain-stride-2-layer-16-grouping-avgpool2d/mm_projector.bin \
-    --tune_mm_mlp_adapter True \
+    --pretrain_mm_mlp_adapter $WEIGHT_PATH/llava-v1.5-7b-pretrain-rvpe/mm_projector.bin \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
@@ -28,9 +27,9 @@ torchrun --nnodes=${NNODES} --nproc_per_node=${GPUS} --master_port=${PORT} \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir $WEIGHT_PATH/llava-v1.5-7b-finetune-stride-2-layer-16-grouping-avgpool2d \
+    --output_dir $WEIGHT_PATH/llava-v1.5-7b-finetune-rvpe \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 1 \
+    --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
@@ -48,7 +47,12 @@ torchrun --nnodes=${NNODES} --nproc_per_node=${GPUS} --master_port=${PORT} \
     --dataloader_num_workers 4 \
     --lazy_preprocess True \
     --report_to wandb \
+    --run_name ft_rvpe \
     --stride $stride \
     --layer $layer \
     --grouping $grouping \
-    --rpe $rpe
+    --rpe $rpe \
+ 1> /data/datasets/jchen293/logs/exp/llava/finetune_ccvl29_rvpe.out \
+ 2> /data/datasets/jchen293/logs/exp/llava/finetune_ccvl29_rvpe.err
+
+sleep 2d
