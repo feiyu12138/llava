c90f8f27bf8a4dc506709092d2ee7a5ee4da3e79
Beckschen <jienengchen01@gmail.com>
2024-05-11 21:48:54 -0400

    script

---
+++ scripts/v1_5/eval/gqa_retrain_progressive_4600.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+gpu_list="${CUDA_VISIBLE_DEVICES:-0}"
+IFS=',' read -ra GPULIST <<< "$gpu_list"
+
+CHUNKS=${#GPULIST[@]}
+
+ROOT_DATA=/data/datasets/jchen293/data/llava_datasets
+ROOT_WEIGHT=/data/datasets/jchen293/weights/llava/checkpoint
+
+name=pool1d-progressive-4600
+SPLIT="llava_gqa_testdev_balanced"
+GQADIR="$ROOT_DATA/eval_luoxin/eval/gqa/data"
+grouping=none
+stride=8
+layer=2
+unified_vpe=False
+ckpt=$ROOT_WEIGHT/llava-v1.5-7b-finetune-stride-$stride-layer-$layer-grouping-avgpool1d-unified_vpe-$unified_vpe-progressive-4600
+
+for IDX in $(seq 0 $((CHUNKS-1))); do
+    CUDA_VISIBLE_DEVICES=${GPULIST[$IDX]} python -m llava.eval.model_vqa_loader \
+        --model-path $ckpt \
+        --question-file $ROOT_DATA/eval_luoxin/eval/gqa/$SPLIT.jsonl \
+        --image-folder $ROOT_DATA/eval_luoxin/eval/gqa/images \
+        --answers-file $ROOT_DATA/eval_luoxin/eval/gqa/answers/$SPLIT/$name/${CHUNKS}_${IDX}.jsonl \
+        --num-chunks $CHUNKS \
+        --chunk-idx $IDX \
+        --temperature 0 \
+        --conv-mode vicuna_v1 \
+        --grouping $grouping \
+        --stride $stride \
+        --layer $layer &
+done
+
+wait
+
+output_file=$ROOT_DATA/eval_luoxin/eval/gqa/answers/$SPLIT/$name/merge.jsonl
+
+# Clear out the output file if it exists.
+> "$output_file"
+
+# Loop through the indices and concatenate each file.
+for IDX in $(seq 0 $((CHUNKS-1))); do
+    cat $ROOT_DATA/eval_luoxin/eval/gqa/answers/$SPLIT/$name/${CHUNKS}_${IDX}.jsonl >> "$output_file"
+done
+
+python scripts/convert_gqa_for_eval.py --src $output_file --dst $GQADIR/testdev_balanced_predictions.json
+
+cd $GQADIR
+python eval/eval.py --tier testdev_balanced
+
+sleep 2d
---
+++ scripts/v1_5/eval/mmbench_cn_progressive_4600.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+export CUDA_VISIBLE_DEVICES=1
+ROOT_DATA=/data/datasets/jchen293/data/llava_datasets
+ROOT_WEIGHT=/data/datasets/jchen293/weights/llava/checkpoint
+
+SPLIT="llava_gqa_testdev_balanced"
+GQADIR="$ROOT_DATA/eval_luoxin/eval/gqa/data"
+grouping=none
+stride=8
+layer=2
+unified_vpe=False
+ckpt=$ROOT_WEIGHT/llava-v1.5-7b-finetune-stride-$stride-layer-$layer-grouping-avgpool1d-unified_vpe-$unified_vpe-progressive-4600
+name=1dpoolprogressive4600
+
+SPLIT="mmbench_dev_cn_20231003"
+
+python -m llava.eval.model_vqa_mmbench \
+    --model-path $ckpt \
+    --question-file $ROOT_DATA/eval_luoxin/eval/mmbench/$SPLIT.tsv \
+    --answers-file $ROOT_DATA/eval_luoxin/eval/mmbench/answers/$SPLIT/$name.jsonl \
+    --lang cn \
+    --single-pred-prompt \
+    --temperature 0 \
+    --conv-mode vicuna_v1
+
+mkdir -p $ROOT_DATA/eval_luoxin/eval/mmbench/answers_upload/$SPLIT
+
+python scripts/convert_mmbench_for_submission.py \
+    --annotation-file $ROOT_DATA/eval_luoxin/eval/mmbench/$SPLIT.tsv \
+    --result-dir $ROOT_DATA/eval_luoxin/eval/mmbench/answers/$SPLIT \
+    --upload-dir $ROOT_DATA/eval_luoxin/eval/mmbench/answers_upload/$SPLIT \
+    --experiment $name
