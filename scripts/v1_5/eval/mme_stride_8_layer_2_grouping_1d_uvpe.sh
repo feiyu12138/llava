a0d03defc2b794743d2f900288ee55ea1836f471
Beckschen <jienengchen01@gmail.com>
2024-05-13 12:33:08 -0400

    file

--- scripts/v1_5/eval/mme_stride_8_layer_2_grouping_1d_uvpe.sh
+++ scripts/v1_5/eval/mme_stride_8_layer_2_grouping_1d_uvpe.sh
@@ -3,13 +3,17 @@ export CUDA_VISIBLE_DEVICES=0
 layer=2
 stride=8
 grouping=avgpool1d
-name=llava-v1.5-7b-stride-$stride-layer-$layer-grouping-$grouping-uvpe
+name=llava-v1.5-7b-stride-$stride-layer-$layer-grouping-$grouping-uvpe-new
 CKPT="/home/lye21/LLaVA/checkpoints/llava-v1.5-7b-$name"
+
+ROOT_DATA=/data/datasets/jchen293/data/llava_datasets
+ROOT_WEIGHT=/data/datasets/jchen293/weights/llava/checkpoint
+
 python -m llava.eval.model_vqa_loader \
     --model-path liuhaotian/llava-v1.5-7b \
-    --question-file ./playground/data/eval/MME/llava_mme.jsonl \
-    --image-folder ./playground/data/eval/MME/MME_Benchmark_release_version \
-    --answers-file ./playground/data/eval/MME/answers/$name.jsonl \
+    --question-file $ROOT_DATA/eval_luoxin/eval/MME/llava_mme.jsonl \
+    --image-folder $ROOT_DATA/eval_luoxin/eval/MME/MME_Benchmark_release_version \
+    --answers-file $ROOT_DATA/eval_luoxin/eval/MME/answers/$name.jsonl \
     --temperature 0 \
     --conv-mode vicuna_v1 \
     --stride $stride \
@@ -17,7 +21,7 @@ python -m llava.eval.model_vqa_loader \
     --grouping $grouping \
     --unified_vpe True
 
-cd ./playground/data/eval/MME
+cd $ROOT_DATA/eval_luoxin/eval/MME
 
 python convert_answer_to_mme.py --experiment $name
 
