0d4edf11dee857a2b102b18f6c2fdc0809e9f918
lye21 <lye21@login.ccvl.jhu.edu>
2024-05-13 09:30:32 -0400

    script

---
+++ scripts/v1_5/eval_slurm/textvqa_progressive_pool8layer0_2600.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+#SBATCH --job-name=textvqa
+#SBATCH --error=/datasets/jchen293/logs/exp/llava_eval/pool8layer0prog2600_textvqa.err
+#SBATCH --output=/datasets/jchen293/logs/exp/llava_eval/pool8layer0prog2600_textvqa.out
+#SBATCH --gpus=1
+#SBATCH --nodes=1
+#SBATCH --partition=main
+
+export CUDA_VISIBLE_DEVICES=0
+
+module purge
+module load conda
+conda activate llava_git
+
+ROOT_DATA=/datasets/jchen293/data/llava_datasets
+ROOT_WEIGHT=/datasets/jchen293/weights/llava/checkpoint
+
+unified_vpe=False
+stride=8
+layer=0
+ckpt=$ROOT_WEIGHT/llava-v1.5-7b-finetune-stride-$stride-layer-$layer-grouping-avgpool1d-unified_vpe-$unified_vpe-progressive
+name=pool8layer0prog2600
+
+python -m llava.eval.model_vqa_loader \
+    --model-path $ckpt \
+    --question-file $ROOT_DATA/eval_luoxin/eval/textvqa/llava_textvqa_val_v051_ocr.jsonl \
+    --image-folder $ROOT_DATA/eval_luoxin/eval/textvqa/train_images \
+    --answers-file $ROOT_DATA/eval_luoxin/eval/textvqa/answers/$name.jsonl \
+    --temperature 0 \
+    --conv-mode vicuna_v1 
+
+python -m llava.eval.eval_textvqa \
+    --annotation-file $ROOT_DATA/eval_luoxin/eval/textvqa/TextVQA_0.5.1_val.json \
+    --result-file $ROOT_DATA/eval_luoxin/eval/textvqa/answers/$name.jsonl
---
+++ scripts/v1_5/eval_slurm/vizwiz_progressive_pool8layer0_2600.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+#SBATCH --job-name=vizwiz
+#SBATCH --error=/datasets/jchen293/logs/exp/llava_eval/pool8layer0prog2600_vizwiz.err
+#SBATCH --output=/datasets/jchen293/logs/exp/llava_eval/pool8layer0prog2600_vizwiz.out
+#SBATCH --gpus=1
+#SBATCH --nodes=1
+#SBATCH --partition=main
+
+export CUDA_VISIBLE_DEVICES=0
+
+module purge
+module load conda
+conda activate llava_git
+
+ROOT_DATA=/datasets/jchen293/data/llava_datasets
+ROOT_WEIGHT=/datasets/jchen293/weights/llava/checkpoint
+
+unified_vpe=False
+stride=8
+layer=0
+ckpt=$ROOT_WEIGHT/llava-v1.5-7b-finetune-stride-$stride-layer-$layer-grouping-avgpool1d-unified_vpe-$unified_vpe-progressive
+name=pool8layer0prog2600
+
+python -m llava.eval.model_vqa_loader \
+    --model-path $ckpt \
+    --question-file $ROOT_DATA/eval_luoxin/eval/vizwiz/llava_test.jsonl \
+    --image-folder $ROOT_DATA/eval_luoxin/eval/vizwiz/test \
+    --answers-file $ROOT_DATA/eval_luoxin/eval/vizwiz/answers//$name.jsonl \
+    --temperature 0 \
+    --conv-mode vicuna_v1
+    
+python scripts/convert_vizwiz_for_submission.py \
+    --annotation-file $ROOT_DATA/eval_luoxin/eval/vizwiz/llava_test.jsonl \
+    --result-file $ROOT_DATA/eval_luoxin/eval/vizwiz/answers//$name.jsonl \
+    --result-upload-file $ROOT_DATA/eval_luoxin/eval/vizwiz/answers_upload//$name.json
