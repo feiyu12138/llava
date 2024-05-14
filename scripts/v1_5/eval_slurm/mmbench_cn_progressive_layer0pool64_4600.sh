cb684883c5a639425edcca8bd4bb27b29f16bc23
lye21 <lye21@login.ccvl.jhu.edu>
2024-05-14 16:56:47 -0400

    data

---
+++ scripts/v1_5/eval_slurm/mmbench_cn_progressive_layer0pool64_4600.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+#SBATCH --job-name=mmbench_cn
+#SBATCH --error=/datasets/jchen293/logs/exp/llava_eval/pool64layer0prog4600_mmbench_cn.err
+#SBATCH --output=/datasets/jchen293/logs/exp/llava_eval/pool64layer0prog4600_mmbench_cn.out
+#SBATCH --gpus=1
+#SBATCH --nodes=1
+#SBATCH --partition=main
+
+export CUDA_VISIBLE_DEVICES=0
+
+module purge
+module load conda
+conda activate llava_git
+
+ROOT_DATA=/datasets/jchen293/data/llava_datasets
+ROOT_WEIGHT=/datasets/jchen293/weights/llava/checkpoint
+
+unified_vpe=False
+grouping=avgpool1d
+stride=64
+layer=0
+pivot=4600
+ckpt=$ROOT_WEIGHT/llava-v1.5-7b-finetune-stride-$stride-layer-$layer-grouping-$grouping-unified_vpe-$unified_vpe-progressive-$pivot
+name=pool64layer0prog4600
+
+SPLIT="mmbench_dev_cn_20231003"
+
+python -m llava.eval.model_vqa_mmbench \
+    --model-path $ckpt \
+    --question-file $ROOT_DATA/eval_luoxin/eval/mmbench/$SPLIT.tsv \
+    --answers-file $ROOT_DATA/eval_luoxin/eval/mmbench/answers/$SPLIT/$name.jsonl \
+    --lang cn \
+    --single-pred-prompt \
+    --temperature 0 \
+    --conv-mode vicuna_v1
+
+mkdir -p playground/data/eval/mmbench/answers_upload/$SPLIT
+
+python scripts/convert_mmbench_for_submission.py \
+    --annotation-file $ROOT_DATA/eval_luoxin/eval/mmbench/$SPLIT.tsv \
+    --result-dir $ROOT_DATA/eval_luoxin/eval/mmbench/answers/$SPLIT \
+    --upload-dir $ROOT_DATA/eval_luoxin/eval/mmbench/answers_upload/$SPLIT \
+    --experiment $name
