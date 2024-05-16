90c906dde490f8df967dd90d424fd38e5e1fa684
lye21 <lye21@login.ccvl.jhu.edu>
2024-05-16 10:58:59 -0400

    update

---
+++ scripts/v1_5/eval_slurm/mmbench_progressive_pool8layer2_3900.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+#SBATCH --job-name=pool8layer2prog3900_mmbench
+#SBATCH --error=/datasets/jchen293/logs/exp/llava_eval/pool8layer2prog3900_mmbench.err
+#SBATCH --output=/datasets/jchen293/logs/exp/llava_eval/pool8layer2prog3900_mmbench.out
+#SBATCH --gpus=1
+#SBATCH --nodes=1
+#SBATCH --partition=main
+
+export CUDA_VISIBLE_DEVICES=0
+
+module purge
+module load conda
+conda activate llava_git
+
+ROOT_DATA=/datasets/jchen293/data/llava_datasets
+ROOT_WEIGHT=/datasets/jchen293/weights/llava/checkpoint
+
+unified_vpe=False
+stride=8
+layer=0
+ckpt=$ROOT_WEIGHT/llava-v1.5-7b-finetune-stride-8-layer-2-grouping-avgpool1d-unified_vpe-False-progressive-3900
+name=pool8layer2prog3900
+
+SPLIT="mmbench_dev_20230712"
+
+python -m llava.eval.model_vqa_mmbench \
+    --model-path $ckpt \
+    --question-file $ROOT_DATA/eval_luoxin/eval/mmbench/$SPLIT.tsv \
+    --answers-file $ROOT_DATA/eval_luoxin/eval/mmbench/answers/$SPLIT/$name.jsonl \
+    --single-pred-prompt \
+    --temperature 0 \
+    --conv-mode vicuna_v1
+
+mkdir -p playground/data/eval/mmbench/answers_upload/$SPLIT
+
+python scripts/convert_mmbench_for_submission.py \
+    --annotation-file $ROOT_DATA/eval_luoxin/eval/mmbench/$SPLIT.tsv \
+    --result-dir $ROOT_DATA/eval_luoxin/eval/mmbench/answers/$SPLIT \
+    --upload-dir $ROOT_DATA/eval_luoxin/eval/mmbench/answers_upload/$SPLIT \
+    --experiment $name
---
+++ scripts/v1_5/eval_slurm/mmvet_progressive_pool8layer2_3900.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+#SBATCH --job-name=pool8layer2prog3900_mmvet
+#SBATCH --error=/datasets/jchen293/logs/exp/llava/pool8layer2prog3900_mmvet.err
+#SBATCH --output=/datasets/jchen293/logs/exp/llava/pool8layer2prog3900_mmvet.out
+#SBATCH --gpus=1
+#SBATCH --nodes=1
+#SBATCH --partition=main
+
+export CUDA_VISIBLE_DEVICES=0
+
+module purge
+module load conda
+conda activate llava_git
+
+ROOT_DATA=/datasets/jchen293/data/llava_datasets
+ROOT_WEIGHT=/datasets/jchen293/weights/llava/checkpoint
+
+unified_vpe=False
+stride=8
+layer=0
+ckpt=$ROOT_WEIGHT/llava-v1.5-7b-finetune-stride-8-layer-2-grouping-avgpool1d-unified_vpe-False-progressive-3900
+name=pool8layer2prog3900
+
+python -m llava.eval.model_vqa \
+    --model-path $ckpt \
+    --question-file $ROOT_DATA/eval_luoxin/eval/mm-vet/llava-mm-vet.jsonl \
+    --image-folder $ROOT_DATA/eval_luoxin/eval/mm-vet/images \
+    --answers-file $ROOT_DATA/eval_luoxin/eval/mm-vet/answers/$name.jsonl \
+    --temperature 0 \
+    --conv-mode vicuna_v1
+
+mkdir -p $ROOT_DATA/eval_luoxin/eval/mm-vet/results
+
+python scripts/convert_mmvet_for_eval.py \
+    --src $ROOT_DATA/eval_luoxin/eval/mm-vet/answers/$name.jsonl \
+    --dst $ROOT_DATA/eval_luoxin/eval/mm-vet/results/$name.json
+
