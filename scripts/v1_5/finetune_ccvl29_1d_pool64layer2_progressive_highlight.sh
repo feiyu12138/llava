f496c95fd6911f32cabc2901ad29074a51951a6f
Beckschen <jienengchen01@gmail.com>
2024-05-17 15:19:01 -0400

    update

--- scripts/v1_5/finetune_ccvl29_1d_pool64layer2_progressive_highlight.sh
+++ scripts/v1_5/finetune_ccvl29_1d_pool64layer2_progressive_highlight.sh
@@ -71,7 +71,7 @@ deepspeed llava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir  $ROOT_WEIGHT/llava-v1.5-7b-finetune-stride-$stride-layer-$layer-grouping-$grouping-unified_vpe-$unified_vpe-progressive-hightlight \
+    --output_dir  $ROOT_WEIGHT/1dpool64layer2proghighlight \
     --num_train_epochs 1 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
