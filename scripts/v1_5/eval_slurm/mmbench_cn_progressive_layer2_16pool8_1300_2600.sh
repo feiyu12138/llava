a46cc17ef3a73f1370d7b1d47c37242e02b4ad6a
lye21 <lye21@login.ccvl.jhu.edu>
2024-05-18 17:13:44 -0400

    script

---
+++ scripts/v1_5/eval_slurm/mmbench_cn_progressive_layer2_16pool8_1300_2600.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+#SBATCH --job-name=1dpool8layer2_16pivot1300_2600prog_mmbench_cn
+#SBATCH --error=/datasets/jchen293/logs/exp/llava_eval/1dpool8layer2_16pivot1300_2600prog_mmbench_cn.err
+#SBATCH --output=/datasets/jchen293/logs/exp/llava_eval/1dpool8layer2_16pivot1300_2600prog_mmbench_cn.out
+#SBATCH --gpus=1
+#SBATCH --nodes=1
+#SBATCH --partition=main
+
+
+
+module purge
+module load conda
+conda activate llava_git
+
+ROOT_DATA=/datasets/jchen293/data/llava_datasets
+ROOT_WEIGHT=/datasets/jchen293/weights/llava/checkpoint
+
+ckpt=$ROOT_WEIGHT/llava-v1.5-7b-1dpool8layer2_16pivot1300_2600prog
+name=1dpool8layer2_16pivot1300_2600prog
+
+SPLIT="mmbench_dev_cn_20231003"
+CUDA_VISIBLE_DEVICES=0 python -m llava.eval.model_vqa_mmbench \
+    --model-path $ckpt \
+    --question-file $ROOT_DATA/eval_luoxin/eval/mmbench/$SPLIT.tsv \
+    --answers-file $ROOT_DATA/eval_luoxin/eval/mmbench/answers/$SPLIT/$name.jsonl \
+    --lang cn \
+    --single-pred-prompt \
+    --temperature 0 \
+    --conv-mode vicuna_v1
+
+mkdir -p playground/data/eval/mmbench/answers_upload/$SPLIT
+
+python scripts/convert_mmbench_for_submission.py \
+    --annotation-file $ROOT_DATA/eval_luoxin/eval/mmbench/$SPLIT.tsv \
+    --result-dir $ROOT_DATA/eval_luoxin/eval/mmbench/answers/$SPLIT \
+    --upload-dir $ROOT_DATA/eval_luoxin/eval/mmbench/answers_upload/$SPLIT \
+    --experiment $name 
+
+
