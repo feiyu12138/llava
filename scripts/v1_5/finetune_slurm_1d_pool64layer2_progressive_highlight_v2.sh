1cc7f0d2c07c64139a32aba8e0903a8efb7f8b28
lye21 <lye21@login.ccvl.jhu.edu>
2024-05-19 12:17:51 -0400

    update

---
+++ scripts/v1_5/finetune_slurm_1d_pool64layer2_progressive_highlight_v2.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+#
+#SBATCH --job-name=1dpool64layer2proghighlight_v2
+#SBATCH --error=/datasets/jchen293/logs/exp/llava/1dpool64layer2proghighlight_v2.err
+#SBATCH --output=/datasets/jchen293/logs/exp/llava/1dpool64layer2proghighlight_v2.out
+#SBATCH --gpus=8
+#SBATCH --nodes=1
+#SBATCH --partition=main
+#SBATCH --exclude=ccvl[14,33-38]
+#SBATCH --cpus-per-task=80
+
+export WANDB_API_KEY='46e587ae4112a04da96b68ba807395204be787c9'
+export WANDB_PROJECT='llava_team'
+export WANDB_ENTITY='jchen293'
+
+ROOT_DATA=/datasets/jchen293/data/llava_datasets
+ROOT_WEIGHT=/datasets/jchen293/weights/llava/checkpoint
+
+layer=2
+stride=64
+grouping=avgpool1d
+unified_vpe=False
+progressive=True
+
+
+# deepspeed llava/train/train_mem.py \
+#     --deepspeed ./scripts/zero2.json \
+#     --model_name_or_path lmsys/vicuna-7b-v1.5 \
+#     --version plain \
+#     --data_path /data/datasets/jchen293/data/llava_datasets/LLaVA-Pretrain/blip_laion_cc_sbu_558k.json \
+#     --image_folder /data/datasets/jchen293/data/llava_datasets/LLaVA-Pretrain/images \
+#     --vision_tower openai/clip-vit-large-patch14-336 \
+#     --mm_projector_type mlp2x_gelu \
+#     --tune_mm_mlp_adapter True \
+#     --mm_vision_select_layer -2 \
+#     --mm_use_im_start_end False \
+#     --mm_use_im_patch_token False \
+#     --bf16 True \
+#     --output_dir  /data/datasets/jchen293/weights/llava/checkpoint/llava-v1.5-7b-pretrain-stride-$stride-layer-$layer-grouping-$grouping-unified_vpe-$unified_vpe \
+#     --num_train_epochs 1 \
+#     --per_device_train_batch_size 32 \
+#     --per_device_eval_batch_size 4 \
+#     --gradient_accumulation_steps 1 \
+#     --evaluation_strategy "no" \
+#     --save_strategy "steps" \
+#     --save_steps 24000 \
+#     --save_total_limit 1 \
+#     --learning_rate 1e-3 \
+#     --weight_decay 0. \
+#     --warmup_ratio 0.03 \
+#     --lr_scheduler_type "cosine" \
+#     --logging_steps 1 \
+#     --tf32 True \
+#     --model_max_length 2048 \
+#     --gradient_checkpointing True \
+#     --dataloader_num_workers 4 \
+#     --lazy_preprocess True \
+#     --report_to wandb \
+#     --stride $stride \
+#     --layer $layer \
+#     --grouping $grouping \
+#     --unified_vpe $unified_vpe \
+#     > /data/datasets/jchen293/logs/exp/llava/pt_$grouping-stride-$stride-layer-$layer-uvpe-$unified_vpe.log
+
+
+deepspeed llava/train/train_mem.py \
+    --deepspeed ./scripts/zero3.json \
+    --model_name_or_path lmsys/vicuna-7b-v1.5 \
+    --version v1 \
+    --data_path $ROOT_DATA/LLaVA-Tuning/json/llava_v1_5_mix665k_second_half.json \
+    --data_path_ext $ROOT_DATA/LLaVA-Tuning/json/llava_v1_5_mix665k_first_half.json \
+    --image_folder $ROOT_DATA/LLaVA-Tuning \
+    --vision_tower openai/clip-vit-large-patch14-336 \
+    --pretrain_mm_mlp_adapter $ROOT_WEIGHT/llava-v1.5-7b-pretrain-reprod/mm_projector.bin \
+    --mm_projector_type mlp2x_gelu \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --image_aspect_ratio pad \
+    --group_by_modality_length True \
+    --bf16 True \
+    --output_dir  $ROOT_WEIGHT/llava-v1.5-7b-1dpool64layer2proghighlight_v2 \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 50000 \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 4 \
+    --lazy_preprocess True \
+    --report_to wandb \
+    --run_name 1dpool64layer2proghighlight_v2 \
+    --stride $stride \
+    --layer $layer \
+    --grouping $grouping \
+    --unified_vpe $unified_vpe \
+    --progressive $progressive \
+    # 1> /data/datasets/jchen293/logs/exp/llava/1dpool64layer2proghighlight_v2.out \
+    # 2> /data/datasets/jchen293/logs/exp/llava/1dpool64layer2proghighlight_v2.err
+
+sleep 2d
